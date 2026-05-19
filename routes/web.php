6cf77d98c81b61c25b292c5240a9aadbc8b3172c
Guilherme De Araújo Martins Sabino <gamswebdesigner@gmail.com>
2026-05-18 23:29:24 -0300

    feat: configure web routes

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::view('/login', 'auth.login')->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth')->group(function () {
+    Route::get('/', 'home')->name('home');
+});
