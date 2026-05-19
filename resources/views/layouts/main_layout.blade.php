bf634e6d3ff4a69646a6fca4689d26c81f93c08a
Guilherme De Araújo Martins Sabino <gamswebdesigner@gmail.com>
2026-05-18 23:09:37 -0300

    feat: create layouts

---
+++ resources/views/layouts/main_layout.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Notes</title>
+    <link rel="stylesheet" href="{{ assets('assets/bootstrap/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ assets('assets/fontawesome/css/all.min.css') }}">
+    <link rel="shortcut icon" href="{{ assets('assets/images/favicon.png') }}" type="image/png">
+</head>
+
+<body>
+    @yield('content')
+    <script src="{{ assets('assets/bootstrap/bootstrap.bundle.min.js') }}"></script>
+</body>
+
+</html>
