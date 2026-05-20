80bb51ea99d08d7bf9b0ce270c30ece435d1a957
Guilherme De Araújo Martins Sabino <gamswebdesigner@gmail.com>
2026-05-20 12:49:00 -0300

    fix: note routes

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ Route::middleware('guest')->group(function () {
 Route::middleware('auth')->group(function () {
     Route::get('/', [NoteController::class, 'index'])->name('home');
     Route::redirect('/home', 'home');
-    Route::get('/note', [NoteController::class, 'note'])->name('note');
     Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+    Route::get('/new-note', [NoteController::class, 'note'])->name('newNote');
+    Route::get('/{user}/note', [NoteController::class, 'note'])->name('editNote');
+    Route::post('/save', [NoteController::class, 'save'])->name('save');
 });
