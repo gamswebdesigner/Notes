c81affe3f94a75f5b13f65e774b55c907770c4cc
Guilherme De Araújo Martins Sabino <gamswebdesigner@gmail.com>
2026-05-20 13:07:52 -0300

    fix: home routes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,7 +10,7 @@
                 <div class="row mt-5">
                     <div class="col text-center">
                         <p class="display-6 mb-5 text-secondary opacity-50">You have no notes available!</p>
-                        <a href="{{ route('note') }}" class="btn btn-secondary btn-lg p-3 px-5">
+                        <a href="{{ route('newNote') }}" class="btn btn-secondary btn-lg p-3 px-5">
                             <i class="fa-regular fa-pen-to-square me-3"></i>Create Your First Note
                         </a>
                     </div>
@@ -21,7 +21,7 @@
 
                 <!-- notes are available -->
                 <div class="d-flex justify-content-end mb-3">
-                    <a href="#" class="btn btn-secondary px-3">
+                    <a href="{{ route('newNote') }}" class="btn btn-secondary px-3">
                         <i class="fa-regular fa-pen-to-square me-2"></i>New Note
                     </a>
                 </div>
